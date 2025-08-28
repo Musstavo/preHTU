06e2c2b53a4f2f9d96fd95c909f1b3e8d6d67596
Musstavo <mohdtantour@gmail.com>
2025-08-28 22:26:00 +0300

    kata 6 (missing letter in an arr)

--- testing.py
+++ testing.py
@@ -1,2 +1,37 @@
-for n in range(8):
-    print(n)
+#Find the missing letter
+
+#Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+#You will always get a valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+#The array will always contain letters in only one case.
+
+#Example:
+
+#['a','b','c','d','f'] -> 'e'
+#['O','Q','R','S'] -> 'P'
+
+#(Use the English alphabet with 26 letters!)
+import string
+
+def find_missing_letter(letters_arr):
+    missing = ""
+    alphabet = list(string.ascii_letters)
+
+    #slicing attempt SUCCESS ( NEED TO FIND THE MISSING LETTER NOW )
+    str_alphabet = "".join(alphabet)
+    str_letters_arr = "".join(letters_arr)
+
+    alph_sliced = str_alphabet[alphabet.index(str_letters_arr[0]):alphabet.index(str_letters_arr[-1]) + 1]
+    print(alph_sliced)
+
+
+    #for i,char in enumerate(alphabet):
+    #    if alphabet[i] in letters_arr:
+    #      continue
+    #  if alphabet[i] != letters_arr[i]:
+
+
+find_missing_letter(['O','Q','R','S'])
+
+
+
