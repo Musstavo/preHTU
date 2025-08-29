4a3320332c3652964646fb27dab5552474d911e2
Musstavo <mohdtantour@gmail.com>
2025-08-29 22:57:35 +0300

    pffffffffffff difficult

--- testing.py
+++ testing.py
@@ -1,26 +1,11 @@
-    #Write a function that takes an array of numbers (integers for the tests) and a target number.
-    # It should find two different items in the array that, when added together, give the target value.
-    # The indexes of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+#Implement a function that computes the difference between two lists.
+# The function should remove all occurrences of elements from the first list (a) that are present in
+# the second list (b). The order of elements in the first list should be preserved in the result.
 
-    #For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+#Examples
 
-    #The input will always be valid (numbers will be an array of length 2 or greater,
-    # and all of the items will be numbers; target will always be the sum of two different items from that array).
+#If a = [1, 2] and b = [1], the result should be [2].
 
-    #two_sum([1, 2, 3], 4) # returns (0, 2) or (2, 0)
-    #two_sum([3, 2, 4], 6) # returns (1, 2) or (2, 1)
-
-def two_sum(numbers, target):
-    summation = []
-    for n in numbers:
-        for j in numbers:
-            if n!=j:
-                if j + n == target:
-                    summation.append(numbers.index(n))
+#If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
 
 
-    print(tuple(summation))
-
-
-
-two_sum([2, 2, 3], 4)
