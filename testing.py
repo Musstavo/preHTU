ec5e3380436956fc5ee1966549fe9cd37a2aed97
Musstavo <mohdtantour@gmail.com>
2025-08-29 17:09:54 +0300

    roman_numerals.. BIG SITUATION

--- testing.py
+++ testing.py
@@ -1,73 +1,26 @@
-#Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
-# You don't need to validate the form of the Roman numeral.
+    #Write a function that takes an array of numbers (integers for the tests) and a target number.
+    # It should find two different items in the array that, when added together, give the target value.
+    # The indexes of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
 
-#Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately,
-# starting with the leftmost digit and skipping any 0s.
-# So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII).
-# The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
-#Example:
+    #For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
 
-#"MM"      -> 2000
-#"MDCLXVI" -> 1666
-#"M"       -> 1000
-#"CD"      ->  400
-#"XC"      ->   90
-#"XL"      ->   40
-#"I"       ->    1
+    #The input will always be valid (numbers will be an array of length 2 or greater,
+    # and all of the items will be numbers; target will always be the sum of two different items from that array).
 
-#Help:
-#Symbol    Value
-#I          1
-#V          5
-#X          10
-#L          50
-#C          100
-#D          500
-#M          1,000
+    #two_sum([1, 2, 3], 4) # returns (0, 2) or (2, 0)
+    #two_sum([3, 2, 4], 6) # returns (1, 2) or (2, 1)
+
+def two_sum(numbers, target):
+    summation = []
+    for n in numbers:
+        for j in numbers:
+            if n!=j:
+                if j + n == target:
+                    summation.append(numbers.index(n))
 
 
-# ACTUALLY DIFFICULT :'(
-# so if we say "DC" it will == 600 because D > C, but "CD" will == 400 because C < D (WEIRD AF)
-# step-by-step and it will be solved.. hopefully
-
-def sol(roman_numeral):
-    result = 0
-    roman_dict = {"I":1,
-                  "V":5,
-                  "X":10,
-                  "L":50,
-                  "C":100,
-                  "D":500,
-                  "M":1000}
-
- # FAILED TRY due to hard coding ( LOOPING THROUGH THE DICT ) NO POINT IT SEEMS.
- #   for i,k in enumerate(roman_dict):
- #       if i < len(roman_numeral)-1:
-
- #           if roman_dict[roman_numeral[i+1]] < roman_dict[roman_numeral[i]]:
- #              result += roman_dict[roman_numeral[i+1]] + roman_dict[roman_numeral[i]]
- #           if roman_dict[roman_numeral[i+1]] > roman_dict[roman_numeral[i]]:
- #              result += roman_dict[roman_numeral[i+1]] - roman_dict[roman_numeral[i]]
-
- #   if len(roman_numeral) == 1:
- #        result += roman_dict[roman_numeral]
- #   print(result)
+    print(tuple(summation))
 
 
 
-# second try, ITERATE THE ROMAN_NUMERAL NOT THE DICT (so far so good, now the exceptions --> X L C D
-    for i,letter in enumerate(roman_numeral):
-        key = roman_numeral[i]
-        result+=roman_dict[key]
-        if roman_dict[key]<:
-
-    print(result)
-
-
-
-
-    #okay the plan for tmr is as such, we'll do a loop from the maximum value to the least
-    # (or opposite but it's gonna be a pain in the ass) and then check if one is larger than the other (D and C situation)
-    # so basically no need for a billion if statements if you know what you're doing
-
-sol("MM")
+two_sum([2, 2, 3], 4)
