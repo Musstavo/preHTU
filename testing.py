5ef3929271b6e97a43e728f33932c383ff5a7591
Musstavo <mohdtantour@gmail.com>
2025-08-30 18:13:42 +0300

    pffffffffffff difficult

--- testing.py
+++ testing.py
@@ -1,11 +1,59 @@
-#Implement a function that computes the difference between two lists.
-# The function should remove all occurrences of elements from the first list (a) that are present in
-# the second list (b). The order of elements in the first list should be preserved in the result.
+#You live in the city of Cartesia where all roads are laid out in a perfect grid.
+# You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk.
+# The city provides its citizens with a Walk Generating App on their phones
 
-#Examples
+# -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']).
+# You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block,
 
-#If a = [1, 2] and b = [1], the result should be [2].
+# so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!)
+# and will, of course, return you to your starting point. Return false otherwise.
 
-#If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+#Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only).
+# It will never give you an empty array (that's not a walk, that's standing still!).
+
+
+# A BIT DIFFICULT ( very in fact.. the returning to the og position is a pain )
+
+# BRAIN STORMING
+# wtf ???? the len(array) must be == 10 and returning to my og location just like below
+# if it's ['s','s','n','n','n','s','s','s,'n','n'] i think it should return True
+# OKAY WAIT, if we compare 's' and 'n' alone, and 'w' and 'e' alone.. basically creating a counter
+# that if walk[0] == 'n' and walk[1] == 's' then we add one to the counter, but if we encounter another 'n' we subtract one
+# same thing with 'e' and 'w' i believe..
+
+#first issue: the first index... hard code it?! surely not.
+
+
+#ATTEMPT ONE
+def is_valid_walking(walk):
+    counter = 0
+    for i,direction in enumerate(walk):
+        if len(walk[direction]) == 10:
+            if walk[i] != walk[i+1]:
+                counter += 1
+
+
+        else:
+            print(False)
+
+is_valid_walking(['s','s','n','n','n','s','s','s','n','n'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
