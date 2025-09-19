aa4e7399b58972e844bd35a85a9800c149fa475f
Musstavo <mohdtantour@gmail.com>
2025-09-19 22:45:00 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -20,22 +20,21 @@ def order(sentence):
     new_index = 0
     for f, i in enumerate(sentence):
         if i == " ":
-            empty.append(sentence[new_index:f])
+            empty.append(sentence[new_index:f].strip())
             new_index = f
     empty.append(sentence[f - 1 :])
-    new_empty = empty
+    new_empty = empty.copy()
 
     letter_index = 0
     word_index = 0
     for num in range(len(sentence)):
-        if word_index < len(empty) - 1 and empty[word_index][letter_index] in numbers:
+        if word_index < len(empty) and empty[word_index][letter_index] in numbers:
             new_empty[int(empty[word_index][letter_index]) - 1] = empty[word_index]
             letter_index = 0
             word_index += 1
         else:
             letter_index += 1
-
     print(new_empty)
 
 
-order("is2 Thi1s T4est 3a")
+order("4of Fo1r pe6ople g3ood th5e the2")
