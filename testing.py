e71a55d2894a00dd657e32794f36f5dded8cb68e
Musstavo <mohdtantour@gmail.com>
2025-09-20 12:25:45 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,43 +1,39 @@
-# Your task is to sort a given string.
-# Each word in the string will contain a single number.
-# This number is the position the word should have in the result.
+# You are given an integer array height of length n.
+#  There are n vertical lines drawn
+# such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
-# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+# Find two lines that together with the x-axis form a container,
+#  such that the container contains the most water.
 
-# If the input string is empty, return an empty string.
-# The words in the input String will only contain valid consecutive numbers.
+# Return the maximum amount of water a container can store.
 
-# Examples
-# "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
-# "4of Fo1r pe6ople g3ood th5e the2"  -->"Fo1r the2 g3ood 4of th5e pe6ople"
-# ""  -->  ""
+# Notice that you may not slant the container.
 
 
-def order(sentence):
-    numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    empty = []
-    new_empty = []
-    new_index = 0
-    for f, i in enumerate(sentence):
-        if i == " ":
-            empty.append(sentence[new_index:f].strip())
-            new_index = f
-    empty.append(sentence[new_index + 1 :])
-    new_empty = empty.copy()
+def maxArena(height):
+    accumilater = []
+    areas = []
+    for x, y in enumerate(height, start=1):
+        couple = [x, y]
+        accumilater.append(couple)
+    print(accumilater)
 
-    if len(empty) == 1:
-        return sentence
-
-    letter_index = 0
-    word_index = 0
-    for num in range(len(sentence)):
-        if word_index < len(empty) and empty[word_index][letter_index] in numbers:
-            new_empty[int(empty[word_index][letter_index]) - 1] = empty[word_index]
-            letter_index = 0
-            word_index += 1
+    first_index = 0
+    second_index = 1
+    # while len(areas) != pow(len(accumilater), 2):
+    for c in range(pow(len(accumilater), 2)):
+        if accumilater[first_index][1] > accumilater[second_index][1]:
+            print(
+                abs(accumilater[first_index][0] - accumilater[second_index][0])
+                * (accumilater[first_index][1] - accumilater[second_index][1])
+            )
         else:
-            letter_index += 1
-    return " ".join(new_empty)
+            print(
+                abs(accumilater[first_index][0] - accumilater[second_index][0])
+                * (accumilater[first_index][1])
+            )
+    first_index += 1
+    second_index += 1
 
 
-order("is2 Thi1s T4est 3a")
+maxArena([1, 8, 6, 2, 5, 4, 8, 3, 7])
